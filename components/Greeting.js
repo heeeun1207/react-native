e6f2b306d4a7f6cbc473d72d3a16587a3ceac442
jungheeeun <jungheeeun272@gmail.com>
2023-05-18 22:24:46 +0900

    함수 컴포넌트 불러오기

---
+++ components/Greeting.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+//함수 컴포넌트
+function Greeting(){
+  return (
+    <View>
+        <Text>안녕하세요 함수 컴포넌트 입니다.!</Text>
+    </View>
+  );
+}
+
+export default Greeting;
